ad943f07b9bcd573f003fbc77e0e9e563c10d0b3
bwhitcher <bwhitcher@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-02 14:47:15 +0000

    Removed .First.lib in zzz.R

--- pkg/waveslim/R/zzz.R
+++ pkg/waveslim/R/zzz.R
@@ -1,1 +1,11 @@
-.First.lib <- function(lib, pkg) library.dynam("waveslim", pkg, lib)
+## .First.lib <- function(lib, pkg) library.dynam("waveslim", pkg, lib)
+
+.onAttach <- function(lib, pkg) {
+  txt <- paste("\n",
+               pkg,
+               ": Wavelet Method for 1/2/3D Signals (version = ",
+               packageDescription(pkg, lib)[["Version"]],
+               ")\n",
+               sep="")
+  packageStartupMessage(txt)
+}
